6fc5044a821ab9950bc389a340c793a4a3971464
abhishek <abhishekc838@gmail.com>
2021-04-20 20:07:23 +0530

    my python cc 1

---
+++ cc_1/sum.py
@@ -0,0 +1,6 @@
+
+a = int(input("enter a value "))
+
+b = int(input("enter b value "))
+
+print("total sum " , a+b)
---
+++ cc_1/user_input.py
@@ -0,0 +1,12 @@
+
+user_name = input("enter your name ")
+
+age = int(input("enter your age "))
+
+telephone = int(input("enter your number "))
+
+print("welcome " , user_name)
+
+print("Your Age is:" , age)
+
+print("Your Telephone Number is:" , telephone)
