2bc2302df63c1d5f561e804af7e641f455a72159
abhishek <abhishekc838@gmail.com>
2021-04-22 12:01:45 +0530

    cc 3

---
+++ cc_3/table.py
@@ -0,0 +1,16 @@
+# Take 1 number as input from the user. Print its table from 1 to 10.
+
+a = int(input("give a number "))
+
+print("Table of" , a)
+print(a , "X 1 =" , a*1)
+print(a , "X 2 =" , a*2)
+print(a , "X 3 =" , a*3)
+print(a , "X 4 =" , a*4)
+print(a , "X 5 =" , a*5)
+print(a , "X 6 =" , a*6)
+print(a , "X 7 =" , a*7)
+print(a , "X 8 =" , a*8)
+print(a , "X 9 =" , a*9)
+print(a , "X 10 =" , a*10)
+
---
+++ cc_3/ture_use.py
@@ -0,0 +1,9 @@
+# Take 2 numbers as input from the user and print True if the first
+# number is greater, false otherwise.
+
+num1 = int(input("enter first number "))
+num2 = int(input("enter second number "))
+
+result = num1 > num2
+
+print(result)
