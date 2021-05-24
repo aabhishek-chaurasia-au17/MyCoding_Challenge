53fee034e357b2b5dd4027e8e1830e87800b587f
aabhishek-chaurasia-au17 <abhishekc838@gmail.com>
2021-05-24 13:09:13 +0530

    cc

---
+++ coding-challenges/week07/day02/Q.2.py
@@ -0,0 +1,28 @@
+"""
+Given an sorted integer array . Write a program to find Lower Bound of
+target number, return the index of the element
+Input: arr = [1,2,3,3,3,4,4,5,5,7,7,7] , target = 6
+Output: 8
+Explanation : -
+def Lower_Bound(arr , target):
+#write code here
+"""
+
+def Lower_bound(arr,target):
+    prev = -1
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+        elif arr[i] > target:
+            return prev
+        prev = i    
+    
+    
+if __name__ == "__main__":
+    arr =  [1,2 ,3,3,3,4,4,5,5,7,7,7] 
+    target = 6
+    print(Lower_bound(arr,target))
+
+
+# Time Complexity: O(n). 
+# Space Complexity: O(1). 
